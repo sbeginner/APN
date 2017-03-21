5b252c7550a516e9e3e55cfa65188f78f141dcda
jack <s4851798@gmail.com>
2017-03-21 22:16:43 +0900

    hello world

--- src/main/java/main.java
+++
@@ -1,6 +0,0 @@
-/**
- * Created by jack on 2017/3/19.
- */
-public class main {
-
-}
--- src/test/java/mainTest.java
+++
@@ -1,8 +0,0 @@
-import static org.junit.Assert.*;
-
-/**
- * Created by jack on 2017/3/19.
- */
-public class mainTest {
-
-}
